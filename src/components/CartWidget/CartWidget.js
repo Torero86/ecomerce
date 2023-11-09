3c391aac94b948b4defac787b2d3d1abf302618c
manuelovejeroe <manuelovejeroetchegaray@gmail.com>
2023-11-09 17:55:50 -0300

    PrimeraEntrega

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import cart from "./assets/cat-svgrepo-com.svg"
+
+const CartWidget = () =>{
+    return(
+        <div>
+            <img src={cart} alt = "cart-widget"/>
+            0
+        </div>
+    )
+}
+
+export default CartWidget
